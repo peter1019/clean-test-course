4a737ef5cec2e50d0931c70e122d625fe658139b
peter <petermwita16@gmail.com>
2026-08-13 14:33:05 +0300

    update backend delivery tests and frontend constants feat: update backend coverage and frontend utilities
    test: update backend delivery tests and frontend constants

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -35,3 +35,14 @@ def test_LittleItems():
     cost = Delivery.calculate(order,delivery_distance)
   #Assert
     assert cost == 2.5
+
+def test_LittleItems():
+  #Arrange
+    order = MockSet()
+    order.add(MockModel(quantity=3))
+    order.add(MockModel(quantity=1))
+    delivery_distance = 2
+  #Act
+    cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+    assert cost == 2.50
